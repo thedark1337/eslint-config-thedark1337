127a199564e123ade3770c6574ba939d0d96252c
thedark1337 <thedark1337@thedark1337.com>
2015-10-14 16:20:21 -0500

    Updated Tests to reflect changes.Utilize Const / Let , and remove util function

--- test/test.js
+++ test/test.js
@@ -1,20 +1,14 @@
 'use strict';
 
-var chai = require('chai');
-var errors;
-var eslint = require('eslint');
-var expect = chai.expect;
-var path = require('path');
-var tempWrite = require('temp-write');
-
-function deleteCache() {
-    Object.keys(require.cache).forEach(function(module) {
-        delete require.cache[require.resolve(module)];
-    });
-}
+const chai = require('chai');
+let errors;
+const eslint = require('eslint');
+const expect = chai.expect;
+const path = require('path');
+const tempWrite = require('temp-write');
 
 function runLint(str, conf) {
-    var linter = new eslint.CLIEngine({
+    const linter = new eslint.CLIEngine({
         useEslintrc: false,
         configFile: tempWrite.sync(JSON.stringify(conf))
     });
@@ -22,20 +16,22 @@ function runLint(str, conf) {
 }
 
 describe('Main Rules Test', function() {
-    deleteCache();
-    var mainConf = require('../');
+    const mainConf = require('../');
 
     it('Should be objects', function() {
+        expect(mainConf.ecmaFeatures).to.be.an('object');
         expect(mainConf.env).to.be.an('object');
         expect(mainConf.rules).to.be.an('object');
     });
 
     describe('Linting Errors', function() {
         it('Should return errors for indentation, quotes, and semicolons', function() {
-            errors = runLint('\'use strict\';\n console.log("thedark1337Wins")\n', mainConf);
-            expect(errors[0].ruleId).to.equal('indent');
-            expect(errors[1].ruleId).to.equal('quotes');
-            expect(errors[2].ruleId).to.equal('semi');
+            this.timeout(5000);
+            errors = runLint('\'use strict\';\n console.log("thedark1337 Wins")\n', mainConf);
+            expect(errors[0].ruleId).to.equal('strict');
+            expect(errors[1].ruleId).to.equal('indent');
+            expect(errors[2].ruleId).to.equal('quotes');
+            expect(errors[3].ruleId).to.equal('semi');
         });
     })
 
@@ -43,11 +39,12 @@ describe('Main Rules Test', function() {
 
 
 describe('Browser Rules Test', function() {
-    deleteCache();
-    var browserConf = require('../browser.js');
+    const browserConf = require('../browser.js');
 
     it('Should define all the right environments', function() {
         expect(browserConf.env).to.deep.equal({
+            browser: true,
+            es6: true,
             jquery: true,
             node: false
         })
@@ -55,18 +52,19 @@ describe('Browser Rules Test', function() {
 });
 
 describe('Plug.dj Rules Test', function() {
-    deleteCache();
-    var plugdjConf = require('../plugdj.js');
+    const plugdjConf = require('../plugdj.js');
 
     it('Should define all the right environments', function() {
         expect(plugdjConf.env).to.deep.equal({
             amd: true,
+            browser: true,
+            es6: true,
             jquery: true,
             node: false
         });
     });
 
-    it('Should not error on global var API', function() {
+    it('Should not error on global const API', function() {
         errors = runLint("'use strict';\nconsole.log(API.BAN.HOUR);\n", plugdjConf);
         expect(errors).to.be.empty;
     });
@@ -78,14 +76,12 @@ describe('Plug.dj Rules Test', function() {
 });
 
 describe('ES6 Rules Test', function() {
-    deleteCache();
-    var es6Conf = require('../esnext.js');
+    const es6Conf = require('../esnext.js');
     it('Should catch syntax errors', function() {
-        errors = runLint('class Foo { function() {\n    const x = 0;\n    const y = 0;\n    const z = 0;\n    var foo = 0;\n    console.log(foo);\n    let bar = {\n        x: x,\n        y: y,\n        z: z\n    };\n    console.log(bar);\n} }\n', es6Conf);
-            expect(errors[0].ruleId).to.equal('no-unused-vars');
-            expect(errors[1].ruleId).to.equal('no-var');
-            expect(errors[2].ruleId).to.equal('prefer-const');
-            expect(errors[3].ruleId).to.equal('object-shorthand');
+        this.timeout(5000);
+        errors = runLint('export class Foo { function() {\n    const x = 0;\n    const y = 0;\n    const z = 0;\n    const foo = 0;\n    console.log(foo);\n    let bar = {\n        x: x,\n        y: y,\n        z: z\n    };\n    console.log(bar);\n} }\n', es6Conf);
+            expect(errors[0].ruleId).to.equal('prefer-const');
+            expect(errors[1].ruleId).to.equal('babel/object-shorthand');
     });
 
 });
