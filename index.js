8387858ddb7630fa028405e30e0f9f2157f74188
thedark1337 <thedark1337@thedark1337.com>
2015-11-24 02:36:34 -0600

    Update: Config now has all ESLint rules added.
    As of Version 1.0.0 all ESLint rules will be added as new ones come out.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     ecmaFeatures: {
+        blockBindings: true,
         modules: true,
         jsx: true,
         experimentalObjectRestSpread: true
@@ -12,7 +13,7 @@ module.exports = {
     },
     rules: {
         // Possible Errors
-        'comma-dangle': [2, 'never'],
+        'comma-dangle': 2,
         'no-cond-assign': 2,
         'no-constant-condition': 2,
         'no-control-regex': 2,
@@ -20,6 +21,7 @@ module.exports = {
         'no-dupe-args': 2,
         'no-dupe-keys': 2,
         'no-duplicate-case': 2,
+        'no-empty': 2,
         'no-empty-character-class': 2,
         'no-ex-assign': 2,
         'no-extra-boolean-cast': 2,
@@ -40,28 +42,36 @@ module.exports = {
         // Best Practices
         'accessor-pairs': 2,
         'block-scoped-var': 2,
-        'curly': [2, 'multi-line'],
+        complexity: 0,
+        'consistent-return': 0,
+        curly: [2, 'multi-line'],
         'default-case': 2,
         'dot-notation': 2,
         'dot-location': [2, 'property'],
-        'eqeqeq': [2, 'smart'],
+        eqeqeq: [2, 'smart'],
         'guard-for-in': 2,
         'no-alert': 2,
         'no-caller': 2,
+        'no-case-declarations': 0,
         'no-div-regex': 2,
         'no-else-return': 2,
+        'no-eq-null': 0,
         'no-empty-label': 2,
+        'no-empty-pattern': 2,
         'no-eval': 2,
         'no-extend-native': 2,
         'no-extra-bind': 2,
+        'no-extra-parens': 0,
         'no-fallthrough': 2,
         'no-floating-decimal': 2,
         'no-implicit-coercion': 2,
         'no-implied-eval': 2,
+        'no-invalid-this': 0,
         'no-iterator': 2,
         'no-labels': 2,
         'no-lone-blocks': 2,
         'no-loop-func': 2,
+        'no-magic-numbers': 0,
         'no-multi-spaces': 2,
         'no-multi-str': 2,
         'no-native-reassign': 2,
@@ -70,6 +80,7 @@ module.exports = {
         'no-new': 2,
         'no-octal-escape': 2,
         'no-octal': 2,
+        'no-process-env': 0,
         'no-proto': 2,
         'no-redeclare': 2,
         'no-return-assign': [2, 'always'],
@@ -83,20 +94,25 @@ module.exports = {
         'no-void': 2,
         'no-warning-comments': 1,
         'no-with': 2,
-        'radix': 2,
+        radix: 2,
         'wrap-iife': [2, 'inside'],
-        'yoda': 2,
+        yoda: 2,
 
-        'strict': [2, 'global'],
+        strict: [2, 'global'],
 
         // Variables
+        'init-declarations': 0,
+        'no-catch-shadow': 0,
+        'no-console': 0,
         'no-delete-var': 2,
         'no-label-var': 2,
+        'no-shadow': 2,
         'no-shadow-restricted-names': 2,
         'no-undef-init': 2,
         'no-undef': [2, {
             typeof: true
         }],
+        'no-undefined': 0,
         'no-unused-vars': [1, {
             vars: 'all',
             args: 'none'
@@ -104,8 +120,8 @@ module.exports = {
         'no-use-before-define': [2, 'nofunc'],
 
         // Node.js
-
-        'global-require': 1,
+        'callback-return': 0,
+        'global-require': 0,
         'handle-callback-err': 1,
         'no-mixed-requires': [2, {
             grouping: true
@@ -113,9 +129,12 @@ module.exports = {
         'no-new-require': 2,
         'no-path-concat': 2,
         'no-process-exit': 2,
+        'no-restricted-modules': 0,
+        'no-sync': 2,
 
         // Stylistic Issues
         'array-bracket-spacing': [2, 'never'],
+        'block-spacing': [2, 'always'],
         'brace-style': [2, '1tbs', {
             allowSingleLine: false
         }],
@@ -128,7 +147,14 @@ module.exports = {
         }],
         'comma-style': [2, 'last'],
         'computed-property-spacing': [2, 'never'],
+        'consistent-this': 0,
         'eol-last': 2,
+        'func-names': 0,
+        'func-style': [2, 'declaration', {
+            allowArrowFunctions: true
+        }],
+        'id-length': 0,
+        'id-match': 0,
         'indent': [2, 4, {
             SwitchCase: 1
         }],
@@ -138,36 +164,49 @@ module.exports = {
             afterColon: true
         }],
         'linebreak-style': [2, 'unix'],
+        'lines-around-comment': [2, {
+            beforeBlockComment: true,
+            beforeLineComment: true
+        }],
         'max-nested-callbacks': [1, 4],
         'new-cap': [2, {
             newIsCap: true,
             capIsNew: true
         }],
         'new-parens': 2,
+        'newline-after-var': 2,
         'no-array-constructor': 2,
+        'no-continue': 0,
+        'no-inline-comments': 0,
         'no-lonely-if': 1,
         'no-mixed-spaces-and-tabs': 2,
         'no-multiple-empty-lines': [2, {
             max: 1
         }],
         'no-negated-condition': 2,
+        'no-nested-ternary': 0,
         'no-new-object': 2,
+        'no-param-reassign': 0,
         'no-restricted-syntax': [2, 'WithStatement'],
         'no-spaced-func': 2,
+        'no-ternary': 0,
         'no-trailing-spaces': 2,
+        'no-underscore-dangle': 0,
         'no-unneeded-ternary': 2,
-        'object-curly-spacing': [2, 'never'],
+        'object-curly-spacing': [2, 'always'],
         'one-var': [2, 'never'],
         'operator-assignment': [2, 'always'],
         'operator-linebreak': [2, 'after'],
         'padded-blocks': [2, 'never'],
         'quote-props': [2, 'as-needed'],
         quotes: [2, 'single', 'avoid-escape'],
+        'require-jsdoc': 0,
         'semi-spacing': [2, {
             before: false,
             after: true
         }],
         'semi': [2, 'always'],
+        'sort-vars': 0,
         'space-after-keywords': [2, 'always'],
         'space-before-keywords': [2, 'always'],
         'space-before-blocks': [2, 'always'],
@@ -186,18 +225,40 @@ module.exports = {
             },
             requireReturn: false
         }],
+        'vars-on-top': 0,
+        'wrap-regex': 0,
+
+        // Legacy
+        'max-depth': [2, 10],
+        'max-len': 0,
+        'max-params': 0,
+        'max-statements': 0,
+        'no-bitwise': 0,
+        'no-plusplus': 0,
 
         // ES2015
-            'arrow-parens': [2, 'as-needed'],
+        'arrow-body-style': 0,
+        'arrow-parens': [2, 'as-needed'],
         'arrow-spacing': [2, {
             before: true,
             after: true
         }],
         'constructor-super': 2,
         'generator-star-spacing': [2, 'both'],
+        'no-arrow-condition': 1,
         'no-class-assign': 2,
         'no-const-assign': 2,
         'no-dupe-class-members': 2,
-        'no-this-before-super': 2
+        'no-this-before-super': 2,
+        'no-var': 2,
+        'object-shorthand': 2,
+        'prefer-arrow-callback': 2,
+        'prefer-const': 2,
+        'prefer-reflect': [2, {
+            exceptions: ['delete']
+        }],
+        'prefer-spread': 2,
+        'prefer-template': 2,
+        'require-yield': 0
     }
 };
