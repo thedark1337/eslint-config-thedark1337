6c8c34ba98afa0c063a467344ccf09fb28a5a7d5
Thedark1337 <thedark1337@thedark1337.com>
2016-03-11 09:20:04 -0600

    Update: remove is-ci and use process.env directly

--- test/lint.js
+++ test/lint.js
@@ -1,8 +1,8 @@
 'use strict';
-
+const isCI = Boolean(process.env.CI || process.env.CONTINUOUS_INTEGRATION);
 const lint = require('mocha-eslint');
 const options = {
-    alwaysWarn: require('is-ci')
+    alwaysWarn: !isCI
 };
 const paths = [
     'browser.js',
