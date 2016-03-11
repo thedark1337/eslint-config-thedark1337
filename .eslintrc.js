722a1a91688e3289e9d02f8fbe63298c9ea8c56c
Thedark1337 <thedark1337@thedark1337.com>
2016-03-11 09:19:46 -0600

    Update: disable newline-per-chained-call for tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,7 @@
-module.exports = require('./');
+module.exports =  {
+    extends: './index.js',
+    rules: {
+        //TODO: enable it again and test when ESLint 2.3.x is usable with babel-eslint.
+        'newline-per-chained-call': 0
+    }
+};
