59bdaa907f9437f90581d309fe3af0c08ed1e66d
Thedark1337 <thedark1337@thedark1337.com>
2017-12-12 02:24:49 -0600

    Update: Removed old rule / todo from a long time ago

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,3 @@
 module.exports =  {
-    extends: './index.js',
-    rules: {
-        //TODO: enable it again and test when ESLint 2.3.x is usable with babel-eslint.
-        'newline-per-chained-call': 0
-    }
+    extends: './index.js'
 };
